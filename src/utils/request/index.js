6a8a286baa80edc0c4bda88e1e4d33379b180914
lamphc <lamphc@live.com>
2020-06-21 19:44:09 +0800

    更改基础API路径

--- src/utils/request/index.js
+++ src/utils/request/index.js
@@ -3,20 +3,20 @@
  * @param {*} param 请求参数
  */
 // 基础地址备用：https://api-ugo-dev.itheima.net
-const BASE_URL = 'https://ugo.botue.com'
-// const BASE_URL = 'https://api-ugo-dev.itheima.net'
-export default async function request({ url, method, data, header }) {
+// const BASE_URL = 'https://ugo.botue.com'
+const BASE_URL = 'https://api-ugo-dev.itheima.net'
+export default async function request ({ url, method, data, header }) {
   uni.showLoading({
     title: '加载中...',
     mask: true
-  });
+  })
   let [error, res] = await uni.request({
     url: BASE_URL + url,
     header,
     method,
     data
-  });
-  uni.hideLoading();
+  })
+  uni.hideLoading()
   if (!error) {
     const _data = {
       msg: res.data.meta,
@@ -27,7 +27,7 @@ export default async function request({ url, method, data, header }) {
 }
 // vue 插件形式
 const MyRequest = {
-  install(Vue, opts) {
+  install (Vue, opts) {
     Vue.prototype.request = request
   }
 }
